35a2b2be1c1827aa776d55a3ff7036e7616b0dbd
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-04-18 16:23:49 +0200

    Write conceptual names for the main functions of the Lottery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.6.6;
 
 contract Lottery {
-    
+	function enter() public {}
+
+	function getEntranceFee() public {}
+
+	function startLottery() public {}
+
+	function endLottery() public {}
 }
