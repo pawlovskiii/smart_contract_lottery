cca046d7e870fd0d91a8ad9ec5aeddc647c0d660
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-04-19 12:01:57 +0200

    Add dependencies to compile the Lottery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
 
