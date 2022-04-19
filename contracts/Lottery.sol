a19d84801f94113feb04f4d436248c619fba4b3c
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-04-19 11:53:05 +0200

    Add constructor with several variables to create enter() function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,10 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
 
-contract Lottery {
-	function enter() public {}
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-	function getEntranceFee() public {}
+contract Lottery {
+
+	address payable[] public players;
+	uint256 public usdEntryFee;
+	AggregatorV3Interface internal ethUsdPriceFeed;
+
+	constructor(address _priceFeedAddress) public {
+		usdEntryFee = 50 * (10 ** 18);
+		ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+	}
+
+	function enter() public payable {
+		// $50 minimum
+		players.push(msg.sender);
+	}
+
+	function getEntranceFee() public view returns (uint256) {}
 
 	function startLottery() public {}
 
