4ef28fd7b995093d36de9e160ccfb010f05d16df
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-04-20 22:45:19 +0200

    Add function about giving entrance fee to the lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
 
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import '@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol';
 
 contract Lottery {
-
 	address payable[] public players;
 	uint256 public usdEntryFee;
 	AggregatorV3Interface internal ethUsdPriceFeed;
 
 	constructor(address _priceFeedAddress) public {
-		usdEntryFee = 50 * (10 ** 18);
+		usdEntryFee = 50 * (10**18);
 		ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
 	}
 
@@ -19,7 +18,19 @@ contract Lottery {
 		players.push(msg.sender);
 	}
 
-	function getEntranceFee() public view returns (uint256) {}
+	function getEntranceFee() public view returns (uint256) {
+		/* 
+		We're skipping SafeMath for simplicity here!
+		*/
+
+		(, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+		uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
+		// $50, $2000 / ETH
+		// 50 / 2000
+		// 50 * 100000 / 2000
+		uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+		return costToEnter;
+	}
 
 	function startLottery() public {}
 
