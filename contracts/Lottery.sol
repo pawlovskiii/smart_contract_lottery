011a78af8e3f8ab9b74639324a5e6df75bbb7a39
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-04-25 23:43:28 +0200

    Add onlyOwner modifier to the startLottery function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.6.6;
 import '@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract Lottery is Ownable {
+contract Lottery is Ownable{
 	address payable[] public players;
 	uint256 public usdEntryFee;
 	AggregatorV3Interface internal ethUsdPriceFeed;
@@ -46,7 +46,7 @@ contract Lottery is Ownable {
 		return costToEnter;
 	}
 
-	function startLottery() public {
+	function startLottery() public onlyOwner {
 		require(
 			lottery_state == LOTTERY_STATE.CLOSED,
 			"Can't start a new lottery yet!"
