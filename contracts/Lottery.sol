bdd7d309628f06cdfbadbb575dbb2ff863894dbd
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-04-28 23:49:06 +0200

    Add solidity file to allow getting a random number inside a smart contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.6.6;
 
 import '@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import '@chainlink/contracts/src/v0.6/VRFConsumerBase.sol';
 
 contract Lottery is Ownable {
 	address payable[] public players;
