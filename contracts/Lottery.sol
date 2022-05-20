622010bd420e695e148644f4b9391331def67255
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-05-20 10:02:41 +0200

    Create variable to keep track of the most recent random number

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,6 +8,7 @@ import '@chainlink/contracts/src/v0.6/VRFConsumerBase.sol';
 contract Lottery is VRFConsumerBase, Ownable {
 	address payable[] public players;
 	address public recentWinner;
+	uint256 public randomness;
 	uint256 public usdEntryFee;
 	AggregatorV3Interface internal ethUsdPriceFeed;
 	enum LOTTERY_STATE {
@@ -86,5 +87,6 @@ contract Lottery is VRFConsumerBase, Ownable {
 		// Reset
 		players = new address payable[](0);
 		lottery_state = LOTTERY_STATE.CLOSED;
+		randomness = _randomness;
 	}
 }
