589985f0e6efdaf5c6f08f118343776f534751b4
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-05-22 21:12:24 +0200

    Copy and paste the get_account function from the previously brownie_fund_me project

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,2 +1,11 @@
+from brownie import network, accounts, config
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
 def get_account():
-    pass
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
