da0a375b3dc445203cb3df9044d136384c5165cb
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-05-23 21:17:50 +0200

    Add two if statements about choosing different accounts depending on the provided parameters

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -5,6 +5,10 @@ LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 
 def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
         return accounts[0]
     return accounts.add(config["wallets"]["from_key"])
