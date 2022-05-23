0705a2a24883791e0a8b417becf0b7cd4a1d9aa6
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-05-23 21:10:23 +0200

    Add two parameters to the get_account function

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -4,7 +4,7 @@ FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 
-def get_account():
+def get_account(index=None, id=None):
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
         return accounts[0]
     return accounts.add(config["wallets"]["from_key"])
