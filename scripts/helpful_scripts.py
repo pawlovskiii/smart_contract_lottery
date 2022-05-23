919d5f63b9a3129cfd00aa7b4273c55753afa35d
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-05-23 20:57:59 +0200

    Delete else statement from the get_account function to set the default expression

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -7,5 +7,4 @@ LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 def get_account():
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
         return accounts[0]
-    else:
-        return accounts.add(config["wallets"]["from_key"])
+    return accounts.add(config["wallets"]["from_key"])
