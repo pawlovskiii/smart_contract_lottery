9a028a9fe9e33d3517431aefebefa97785a4d96c
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-06-09 23:22:43 +0200

    Create a transaction for entering the lottery with provided account and value

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -27,6 +27,9 @@ def enter_lottery():
     account = get_account()
     lottery = Lottery[-1]
     value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You entered the lottery!")
 
 
 def main():
